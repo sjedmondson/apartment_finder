108142f8bc538a60b51e5243275fd618f8cff5c7
vagrant <vagrant@lighthouse-labs>
2016-08-11 22:25:29 +0000

    filter search

--- app/actions.rb
+++ app/actions.rb
@@ -16,6 +16,7 @@ get '/listings/create' do
   erb :'listings/create'
 end
 
+
 post '/listings' do
   @listing = Listing.new(
     price: params[:price],
@@ -25,13 +26,25 @@ post '/listings' do
     bathrooms: params[:bathrooms]
     )
   if @listing.save
-    redirect '/listings'
+    redirect '/listings/map'
   else
     erb :'listing/create'
   end
 end
 
+get '/user/preferences' do
+  erb :'user/preferences'
+end
 
-
+post '/user/preferences' do
+  max_price = (params[:max_price].nil?) ? 1000000 : params[:max_price]
+  min_area = (params[:min_area].nil?) ? 0 : params[:min_area]
+  min_bedrooms = (params[:min_bedrooms].nil?) ? 0 : params[:min_bedrooms]
+  min_bathrooms = (params[:min_bathrooms].nil?) ? 0 : params[:min_bathrooms]
+  @listings = Listing.where(
+    "max_price < ? AND min_area > ? AND min_bedrooms > ? AND min_bathrooms > ?",max_price, min_area, min_bedrooms, min_bathrooms
+    )
+  erb :'listings/map'
+end
 
 
