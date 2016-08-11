d7844e53a5e0707108434b9e800d51a6b7af85d6
Tim Lu <tim.lu@ourlog.ca>
2016-08-11 22:05:26 +0000

    Cleaned up listing form a bit, changed some redirections in actions as well.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,13 +1,33 @@
 class Listing < ActiveRecord::Base
 
-  validates :price,
-    presence: true
-  validates :area,
-    presence: true
   validates :street_address,
     presence: true
+
+  validates :price,
+    numericality: {
+      greater_than: 0,
+    }
+
+  validates :area,
+    numericality: {
+      greater_than: 0,
+    }
+
+  validates :bedrooms,
+    presence: true,
+    numericality: {
+      greater_than: 0,
+    }
+
+  validates :bathrooms,
+    presence: true,
+    numericality: {
+      greater_than: 0
+    }
+
   validates :latitude,
     presence: true
+
   validates :longitude,
     presence: true
 
