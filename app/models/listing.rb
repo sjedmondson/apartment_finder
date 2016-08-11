af8c82196018ac788354a4057744f5a6e45bf625
abkode <abozar.rahimi@gmail.com>
2016-08-11 18:53:48 +0000

    some changes in front end

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,18 @@
 class Listing < ActiveRecord::Base
+  
   validates :price,
     presence: true
   validates :area,
     presence: true
-  validates :url,
+  validates :street_address,
     presence: true
+    
+  # after_validation do
+  #   full_street_address = street_address + ", Vancouver"
+  #   result = Geocoder.search(street_address)
+  #   self.longitude = result[0].longitude
+  #   self.latitude result[0].latitude
+  # end
+
+
 end
