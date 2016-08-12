4e9e9a89f54a4058242429e6a8bc3ec937376477
Simon Edmondson <simonedmondson@live.ca>
2016-08-12 21:47:23 +0000

    Partial progress on "my listings", and compat changes to user-session stuff
    We had some conflict in how the session stores the user_id, and the WIP
     code around notification display, so I had to patch those to make it still run.

--- app/actions.rb
+++ app/actions.rb
@@ -1,23 +1,29 @@
 # Homepage (Root path)
 helpers do
   def current_user
-    @user = User.find(session[:user].id) if session[:user]
+    @user = User.find(session[:user_id]) if session[:user_id]
   end
 
   def get_notifications
     if current_user
-      user = current_user
-      max_price = user.preference.max_price
-      min_area = user.preference.min_area
-      min_bedrooms = user.preference.min_bedrooms
-      min_bathrooms = user.preference.min_bathrooms
-      listings = Listing.where(
-        "price < ? AND area > ? AND bedrooms > ? AND bathrooms > ?", 
-        max_price, 
-        min_area, 
-        min_bedrooms, 
-        min_bathrooms
-      ).order(created_at: :desc).limit(6)
+      if current_user.preference
+        user = current_user
+        puts "what's in user here?"
+        p user
+        max_price = user.preference.max_price
+        min_area = user.preference.min_area
+        min_bedrooms = user.preference.min_bedrooms
+        min_bathrooms = user.preference.min_bathrooms
+        listings = Listing.where(
+          "price < ? AND area > ? AND bedrooms > ? AND bathrooms > ?", 
+          max_price, 
+          min_area, 
+          min_bedrooms, 
+          min_bathrooms
+        ).order(created_at: :desc).limit(6)
+      else
+        listings = Listing.where("1 = 2")
+      end
     end
   end
 end
@@ -43,16 +49,16 @@ get '/listings/create' do
   erb :'listings/create'
 end
 
-get '/listings/edit' do
-  erb :'listings/edit'
-end
+# get '/listings/edit' do
+#   erb :'listings/edit'
+# end
 
-get '/listings/:listing_id' do 
-  @listing = Listing.find(params[:listing_id])
-  @listing.created_at.to_s
-end
+# get '/listings/:listing_id' do 
+#   @listing = Listing.find(params[:listing_id])
+#   @listing.created_at.to_s
+# end
 
-post '/listings' do
+post '/listings/create' do
   @listing = Listing.new(
     price: params[:price],
     area: params[:area],
@@ -72,12 +78,7 @@ get '/user/preferences' do
 end
 
 get '/user/listings' do 
-  if current_user
-    @listings = Listing.where(user_id: current_user.id)
-    "this is the user listings page"
-  else
-    redirect '/user/login'
-  end
+  erb :'user/listings'
 end
 
 post '/listings/map' do
